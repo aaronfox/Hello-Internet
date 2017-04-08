93eaed1f015a334cb27af86531835fc3d88b4e3e
Lukas Armstrong <Lucas.J.Armstrong@gmail.com>
2017-04-08 17:40:48 -0400

    adding BingoArray to Bingo.js

---
+++ js/Bingo.js
@@ -0,0 +1,11 @@
+var BingoArray[] = {"Adelaide", "Freebooting", "Grey sighs into the microphone before a point",
+"Squarespace", "Periodic Videos, Numberphile, or Objectivity", "Grey rant", "The vinyl episode is mentioned",
+"Something relate to Everest", "Fit-o-tron 5000", "\"The Tims\"", "Elon Musk", "Grey gets patriotic about the USA",
+"\"The Reddit\"", "Brady doesn't want a solution to his problem", "Wikipedia vandalization endorsement", "\"The spiritual home of Numberphile\"",
+"Flags", "Grey missed an important thing on Twitter", "Brady story", "Audrey and Lulu", "Harry's Razors",
+"\"Hard as nails\"", "Grey says the word \"underlying\"", "Grey complains about Plane Crash Corner", "Toothbrush developments",
+"Corporate Compensation Corner", "\"Unofficial Official [something]\"", "Listener question corner", "Grey goes out of his way to avoid human interaction",
+"Disappointment with Apple", "Grey picks apart Brady's analogies", "Sportsball corner", "Abrupt ending to the podcast",
+"\"Infuriating\"", "\"Pleasing\"", "\"Experience\"", "\"Vastly\"", "Grey feigning interest and Brady pretending it's genuine",
+"Grey says \"non-zero\"", "Mispronunciation of [Derp] from [Verkastablium]", "Has Grey made a video", "\"Due to, in no small part...\"",
+"Humblebrag", "Brady says \"there's something magical\"", "Grey says \"why Brady?\""};
