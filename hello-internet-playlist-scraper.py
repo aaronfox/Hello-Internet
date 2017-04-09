16a48dc429c7ce9ec6a2dfc2e57ba88132b94edc
aaronfox <aaron.m.fox3@gmail.com>
2017-04-09 09:51:44 -0400

    added comments and fixes to python scraper

--- hello-internet-playlist-scraper.py
+++ hello-internet-playlist-scraper.py
@@ -6,21 +6,21 @@
 
 import requests, bs4, sys, os
 
-
+# get a response from the url containing the hello internet playlist
 res = requests.get('https://www.youtube.com/playlist?list=PLe_b-HAZD1pXZl1UzE7Q9IiYMXKxSG7Lg')
+# ensure that the response is a-okay
 res.raise_for_status()
 
 # convert non-readable characters to question marks
 newResText = (res.text).encode(sys.stdout.encoding, errors='replace')
 
+# convert text parsable html
 youtubeSoup = bs4.BeautifulSoup(newResText, "html.parser")
- 
-# print(youtubeSoup)
- 
+
+# create an array containing all video links
 youtubeElems = youtubeSoup.select('.pl-video-title-link')
- 
-# print(youtubeElems)
- 
+
+# write to this file
 allPlaylistLinks = open('./hello-internet-urls.txt', 'w')
 
 for i in range(len(youtubeElems)):
