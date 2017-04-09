9b51eec3715dc1f0b3bfccfd650515d00cfcc0aa
Lukas Armstrong <Lucas.J.Armstrong@gmail.com>
2017-04-08 22:32:47 -0400

    Working on mainNav

--- js/Bingo.js
+++ js/Bingo.js
@@ -35,12 +35,12 @@ function generateTable(size) {
     var shuffledArray = shuffle(bingoArray);
     var index = 0;
     for (var i = 0; i < size; i++) {
-        $(".bingo-table").append("<div class=\"flex-row\">")
+        $(".bingo-table").append("<div class=\"flex-row\">");
         for (var j = 0; j < size; j++) {
             $(".bingo-table").append("<div class=\"flex-column\" id=\"column-" + index + "\">" + shuffledArray[index] + "</div>");
             index++;
         }
-        $(".bingo-table").append("</div>")
+        $(".bingo-table").append("</div>");
     }
 }
 
