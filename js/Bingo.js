27777993b2ae2713dfdd5a738c2b4f2dc92674a1
aaronfox <aaron.m.fox3@gmail.com>
2017-04-09 09:38:54 -0400

    made page more mobile responsive

--- js/Bingo.js
+++ js/Bingo.js
@@ -1,4 +1,4 @@
-var bingoArray = ["Adelaide", "Freebooting", "Grey sighs into the microphone before a point", "Squarespace", "Periodic Videos, Numberphile, or Objectivity", "Grey rant", "The vinyl episode is mentioned", "Something relate to Everest", "Fit-o-tron 5000", "\"The Tims\"", "Elon Musk", "Grey gets patriotic about the USA", "\"The Reddit\"", "Brady doesn't want a solution to his problem", "Wikipedia vandalization endorsement", "\"The spiritual home of Numberphile\"", "Flags", "Grey missed an important thing on Twitter", "Brady story", "Audrey and Lulu", "Harry's Razors", "\"Hard as nails\"", "Grey says the word \"underlying\"", "Grey complains about Plane Crash Corner", "Toothbrush developments", "Corporate Compensation Corner", "\"Unofficial Official [something]\"", "Listener question corner", "Grey goes out of his way to avoid human interaction", "Disappointment with Apple", "Grey picks apart Brady's analogies", "Sportsball corner", "Abrupt ending to the podcast", "\"Infuriating\"", "\"Pleasing\"", "\"Experience\"", "\"Vastly\"", "Grey feigning interest and Brady pretending it's genuine", "Grey says \"non-zero\"", "Mispronunciation of [Derp] from [Verkastablium]", "Has Grey made a video", "\"Due to, in no small part...\"", "Humblebrag", "Brady says \"there's something magical\"", "Grey says \"why Brady?\""];
+var bingoArray = ["Adelaide", "Freebooting", "Grey sighs into the microphone before a point", "Squarespace", "Periodic Videos, Numberphile, or Objectivity", "Grey rant", "The vinyl episode is mentioned", "Something related to Everest", "Fit-o-tron 5000", "\"The Tims\"", "Elon Musk", "Grey gets patriotic about the USA", "\"The Reddit\"", "Brady doesn't want a solution to his problem", "Wikipedia vandalization endorsement", "\"The spiritual home of Numberphile\"", "Flags", "Grey missed an important thing on Twitter", "Brady story", "Audrey and Lulu", "Harry's Razors", "\"Hard as nails\"", "Grey says the word \"underlying\"", "Grey complains about Plane Crash Corner", "Toothbrush developments", "Corporate Compensation Corner", "\"Unofficial Official [something]\"", "Listener question corner", "Grey goes out of his way to avoid human interaction", "Disappointment with Apple", "Grey picks apart Brady's analogies", "Sportsball corner", "Abrupt ending to the podcast", "\"Infuriating\"", "\"Pleasing\"", "\"Experience\"", "\"Vastly\"", "Grey feigning interest and Brady pretending it's genuine", "Grey says \"non-zero\"", "Mispronunciation of [Derp] from [Verkastablium]", "Has Grey made a video", "\"Due to, in no small part...\"", "Humblebrag", "Brady says \"there's something magical\"", "Grey says \"why Brady?\""];
 
 function shuffle(array) {
   var currentIndex = array.length, temporaryValue, randomIndex;
@@ -21,11 +21,26 @@ function shuffle(array) {
 
 function generateTable(size) {
     var shuffledArray = shuffle(bingoArray);
+    
+    // assign each column a div class so that the columns can be resized properly by their size
+    var diffClass = "";
+    switch (Number(size)) {
+        case 3:
+            diffClass = "easy";
+            break;
+        case 5:
+            diffClass = "medium";
+            console.log("changed diffClass to medium");
+            break;
+        case 7:
+            diffClass = "hard";
+            }
+
     var index = 0;
     for (var i = 0; i < size; i++) {
-        $(".bingo-table").append("<div class=\"flex-row fr-"+ i + "\">");
+        $(".bingo-table").append("<div class=\"flex-row fr-"+ i +  "\">");
         for (var j = 0; j < size; j++) {
-            $(".fr-"+i).append("<div class=\"flex-column\" id=\"column-" + index + "\">" + shuffledArray[index] + "</div>");
+            $(".fr-"+i).append("<div class=\"flex-column " + diffClass + "\"" +  "id=\"column-" + index + "\">" + shuffledArray[index] + "</div>");
             index++;
         }
         $(".bingo-table").append("</div>");
@@ -59,8 +74,6 @@ function checkWinner(eventId, size) {
     return boolArray;
 }
 $(document).ready(function(){
-    
-    var ARRAY_SIZE = 5;
   var ARRAY_SIZE = 5;
   $(".bingo-table").on("click", function(event) {
     var opacity = $("#" + event.target.id).css("opacity");
